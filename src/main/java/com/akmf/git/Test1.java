53439abdffba6309c94d8e21cb2adb0b3f43ba77
akmf <763941102@qq.com>
2021-11-06 19:41:38 +0800

    first commit

---
+++ src/main/java/com/akmf/git/Test1.java
@@ -0,0 +1,13 @@
+package com.akmf.git;
+
+/**
+ * author:akmf
+ *
+ * @create 2021-11-06-19:39
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
