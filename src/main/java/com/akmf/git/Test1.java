0fbbebd52f0b41fe724e85b879ec3bd53535a1e5
SuperFLYwei <38938142+SuperFLYwei@users.noreply.github.com>
2021-11-06 22:45:39 +0800

    Update Test1.java

--- src/main/java/com/akmf/git/Test1.java
+++ src/main/java/com/akmf/git/Test1.java
@@ -13,5 +13,6 @@ public class Test1 {
         System.out.println("hot-fix test");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
